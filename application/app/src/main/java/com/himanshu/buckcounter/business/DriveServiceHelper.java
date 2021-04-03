99788898cad924776e154a1390b877b99c3b0b7a
himansh-sfcc <65016671+himansh-sfcc@users.noreply.github.com>
2021-04-03 21:16:51 +0530

    lambda to anon

--- application/app/src/main/java/com/himanshu/buckcounter/business/DriveServiceHelper.java
+++ application/app/src/main/java/com/himanshu/buckcounter/business/DriveServiceHelper.java
@@ -32,6 +32,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Collections;
+import java.util.concurrent.Callable;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
@@ -51,18 +52,21 @@ public class DriveServiceHelper {
      * Creates a text file in the user's My Drive folder and returns its file ID.
      */
     public Task<String> createFile() {
-        return Tasks.call(mExecutor, () -> {
-            File metadata = new File()
-                    .setParents(Collections.singletonList("root"))
-                    .setMimeType("text/plain")
-                    .setName("Untitled file");
+        return Tasks.call(mExecutor, new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                File metadata = new File()
+                        .setParents(Collections.singletonList("root"))
+                        .setMimeType("text/plain")
+                        .setName("Untitled file");
 
-            File googleFile = mDriveService.files().create(metadata).execute();
-            if (googleFile == null) {
-                throw new IOException("Null result when requesting file creation.");
+                File googleFile = mDriveService.files().create(metadata).execute();
+                if (googleFile == null) {
+                    throw new IOException("Null result when requesting file creation.");
+                }
+
+                return googleFile.getId();
             }
-
-            return googleFile.getId();
         });
     }
 
@@ -70,24 +74,27 @@ public class DriveServiceHelper {
      * Opens the file identified by {@code fileId} and returns a {@link Pair} of its name and
      * contents.
      */
-    public Task<Pair<String, String>> readFile(String fileId) {
-        return Tasks.call(mExecutor, () -> {
-            // Retrieve the metadata as a File object.
-            File metadata = mDriveService.files().get(fileId).execute();
-            String name = metadata.getName();
+    public Task<Pair<String, String>> readFile(final String fileId) {
+        return Tasks.call(mExecutor, new Callable<Pair<String, String>>() {
+            @Override
+            public Pair<String, String> call() throws Exception {
+                // Retrieve the metadata as a File object.
+                File metadata = mDriveService.files().get(fileId).execute();
+                String name = metadata.getName();
 
-            // Stream the file contents to a String.
-            try (InputStream is = mDriveService.files().get(fileId).executeMediaAsInputStream();
-                 BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
-                StringBuilder stringBuilder = new StringBuilder();
-                String line;
+                // Stream the file contents to a String.
+                try (InputStream is = mDriveService.files().get(fileId).executeMediaAsInputStream();
+                     BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
 
-                while ((line = reader.readLine()) != null) {
-                    stringBuilder.append(line);
+                    while ((line = reader.readLine()) != null) {
+                        stringBuilder.append(line);
+                    }
+                    String contents = stringBuilder.toString();
+
+                    return Pair.create(name, contents);
                 }
-                String contents = stringBuilder.toString();
-
-                return Pair.create(name, contents);
             }
         });
     }
@@ -96,17 +103,20 @@ public class DriveServiceHelper {
      * Updates the file identified by {@code fileId} with the given {@code name} and {@code
      * content}.
      */
-    public Task<Void> saveFile(String fileId, String name, String content) {
-        return Tasks.call(mExecutor, () -> {
-            // Create a File containing any metadata changes.
-            File metadata = new File().setName(name);
+    public Task<Void> saveFile(final String fileId, final String name, final String content) {
+        return Tasks.call(mExecutor, new Callable<Void>() {
+            @Override
+            public Void call() throws Exception {
+                // Create a File containing any metadata changes.
+                File metadata = new File().setName(name);
 
-            // Convert content to an AbstractInputStreamContent instance.
-            ByteArrayContent contentStream = ByteArrayContent.fromString("text/plain", content);
+                // Convert content to an AbstractInputStreamContent instance.
+                ByteArrayContent contentStream = ByteArrayContent.fromString("text/plain", content);
 
-            // Update the metadata and contents.
-            mDriveService.files().update(fileId, metadata, contentStream).execute();
-            return null;
+                // Update the metadata and contents.
+                mDriveService.files().update(fileId, metadata, contentStream).execute();
+                return null;
+            }
         });
     }
 
@@ -119,8 +129,12 @@ public class DriveServiceHelper {
      * Developer's Console</a> and be submitted to Google for verification.</p>
      */
     public Task<FileList> queryFiles() {
-        return Tasks.call(mExecutor, () ->
-                mDriveService.files().list().setSpaces("drive").execute());
+        return Tasks.call(mExecutor, new Callable<FileList>() {
+            @Override
+            public FileList call() throws Exception {
+                return mDriveService.files().list().setSpaces("drive").execute();
+            }
+        });
     }
 
     /**
@@ -139,32 +153,35 @@ public class DriveServiceHelper {
      * created by {@link #createFilePickerIntent()} using the given {@code contentResolver}.
      */
     public Task<Pair<String, String>> openFileUsingStorageAccessFramework(
-            ContentResolver contentResolver, Uri uri) {
-        return Tasks.call(mExecutor, () -> {
-            // Retrieve the document's display name from its metadata.
-            String name;
-            try (Cursor cursor = contentResolver.query(uri, null, null, null, null)) {
-                if (cursor != null && cursor.moveToFirst()) {
-                    int nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
-                    name = cursor.getString(nameIndex);
-                } else {
-                    throw new IOException("Empty cursor returned for file.");
+            final ContentResolver contentResolver, final Uri uri) {
+        return Tasks.call(mExecutor, new Callable<Pair<String, String>>() {
+            @Override
+            public Pair<String, String> call() throws Exception {
+                // Retrieve the document's display name from its metadata.
+                String name;
+                try (Cursor cursor = contentResolver.query(uri, null, null, null, null)) {
+                    if (cursor != null && cursor.moveToFirst()) {
+                        int nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+                        name = cursor.getString(nameIndex);
+                    } else {
+                        throw new IOException("Empty cursor returned for file.");
+                    }
                 }
-            }
 
-            // Read the document's contents as a String.
-            String content;
-            try (InputStream is = contentResolver.openInputStream(uri);
-                 BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
-                StringBuilder stringBuilder = new StringBuilder();
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    stringBuilder.append(line);
+                // Read the document's contents as a String.
+                String content;
+                try (InputStream is = contentResolver.openInputStream(uri);
+                     BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        stringBuilder.append(line);
+                    }
+                    content = stringBuilder.toString();
                 }
-                content = stringBuilder.toString();
-            }
 
-            return Pair.create(name, content);
+                return Pair.create(name, content);
+            }
         });
     }
 }
